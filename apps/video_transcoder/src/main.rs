1c561afc92f6ac36a0bed84afe3b7742c816212f
Ethan Van Der Heijden <ethanvanderheijden@gmail.com>
2025-07-28 17:52:11 -0700

    Prepare HTTP proxy for video transcoder.

---
+++ apps/video_transcoder/src/main.rs
@@ -0,0 +1,103 @@
+use anyhow::Result;
+use bytes::Bytes;
+use clap::Parser;
+use http::StatusCode;
+use http_body_util::{BodyExt, Empty, Full, combinators::BoxBody};
+use hyper::{Method, Request, Response, service::service_fn, upgrade::Upgraded};
+use hyper_util::{
+    client::legacy::{Client, connect::Connect},
+    rt::{TokioExecutor, TokioIo},
+    server::conn::auto,
+};
+use tokio::net::{TcpListener, TcpStream};
+
+#[derive(Parser)]
+struct Args {
+    port: u16,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = Args::parse();
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", args.port)).await?;
+
+    let client = Client::builder(TokioExecutor::new()).build_http::<hyper::body::Incoming>();
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let client_2 = client.clone();
+        tokio::spawn(async move {
+            let io = TokioIo::new(stream);
+            let service = service_fn(|req| {
+                let value = client_2.clone();
+                async move { proxy(value.clone(), req).await }
+            });
+
+            if let Err(err) = auto::Builder::new(TokioExecutor::new())
+                .serve_connection_with_upgrades(io, service)
+                .await
+            {
+                eprintln!("Failed to serve connection: {:?}", err);
+            }
+        });
+    }
+}
+
+async fn proxy<C>(
+    client: Client<C, hyper::body::Incoming>,
+    req: Request<hyper::body::Incoming>,
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error>
+where
+    C: Connect + Clone + Send + Sync + 'static,
+{
+    if req.method() == Method::CONNECT {
+        if let Some(addr) = req.uri().authority() {
+            let addr = addr.to_string();
+            tokio::spawn(async move {
+                match hyper::upgrade::on(req).await {
+                    Ok(upgraded) => {
+                        if let Err(e) = tunnel(upgraded, addr).await {
+                            eprintln!("IO error while tunneling: {}", e);
+                        };
+                    }
+                    Err(e) => eprintln!("Failed to upgrade on CONNECT: {}", e),
+                }
+            });
+            Ok(Response::new(empty()))
+        } else {
+            eprintln!("CONNECT host is not socket addr: {:?}", req.uri());
+            let mut resp = Response::new(full("CONNECT must be to a socket address"));
+            *resp.status_mut() = StatusCode::BAD_REQUEST;
+            Ok(resp)
+        }
+    } else {
+        match client.request(req).await {
+            Ok(response) => Ok(response.map(|b| b.boxed())),
+            Err(err) => {
+                eprintln!("Failed to make upstream request: {err}");
+                let mut resp = Response::new(full(format!("Upstream request failed: {err}")));
+                *resp.status_mut() = StatusCode::GATEWAY_TIMEOUT;
+                Ok(resp)
+            }
+        }
+    }
+}
+
+async fn tunnel(upgraded: Upgraded, destination: String) -> Result<()> {
+    let mut io = TokioIo::new(upgraded);
+    let mut upstream = TcpStream::connect(destination).await?;
+    tokio::io::copy_bidirectional(&mut io, &mut upstream).await?;
+    Ok(())
+}
+
+fn empty() -> BoxBody<Bytes, hyper::Error> {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
+
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
+}
